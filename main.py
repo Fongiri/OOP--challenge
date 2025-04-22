bda2bc3b1ba7151cd67c875aa7cdbf626caf2d15
fayiifaith <faithongiri@gmail.com>
2025-04-22 16:10:22 +0300

    Complete digital pet OOP challenge with eat, play, sleep, train functionality

---
+++ main.py
@@ -0,0 +1,18 @@
+from pet import Pet
+
+# Create your pet
+my_pet = Pet("Max")
+print("Creating pet: Max")
+
+# Actions
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+
+# Train some tricks
+my_pet.train("roll over")
+my_pet.train("play dead")
+
+# Display pet status
+my_pet.get_status()
+my_pet.show_tricks()
